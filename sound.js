a8fd5cbdd18ca8b6c1585d6e6d5a3fb4ae5cb1f9
stugee004 <stugee004@gmail.com>
2026-07-14 15:02:31 -0700

    Update sound.js

--- sound.js
+++ sound.js
@@ -1,45 +1,25 @@
-const sounds = {
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
-    function playHit() {
-    const audio = new AudioContext();
+function playHit() {
 
-    const osc = audio.createOscillator();
-    const gain = audio.createGain();
+    const osc = audioContext.createOscillator();
+    const gain = audioContext.createGain();
 
     osc.type = "square";
     osc.frequency.value = 500;
 
-    gain.gain.value = 0.15;
+    gain.gain.value = 0.12;
 
     osc.connect(gain);
-    gain.connect(audio.destination);
+    gain.connect(audioContext.destination);
 
     osc.start();
 
     gain.gain.exponentialRampToValueAtTime(
         0.0001,
-        audio.currentTime + 0.08
+        audioContext.currentTime + 0.08
     );
 
-    osc.stop(audio.currentTime + 0.08);
-}
-
-    score: new Audio("sounds/score.wav"),
-
-    victory: new Audio("sounds/victory.wav"),
-
-    ability: new Audio("sounds/ability.wav")
-
-};
-
-
-function playSound(name){
-
-    if(sounds[name]){
-
-        sounds[name].currentTime = 0;
-        sounds[name].play();
-
-    }
+    osc.stop(audioContext.currentTime + 0.08);
 
 }
