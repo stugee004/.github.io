04d1a9415aa3fa8bc1397d9fdfab8778cf557d4d
stugee004 <stugee004@gmail.com>
2026-07-14 10:15:39 -0700

    Create ball.js

---
+++ ball.js
@@ -0,0 +1,37 @@
+class Ball {
+
+    constructor(){
+
+        this.reset();
+
+        this.radius = 10;
+    }
+
+    reset(){
+
+        this.x = canvas.width/2;
+        this.y = canvas.height/2;
+
+        this.dx = Math.random()>0.5 ? 6 : -6;
+        this.dy = (Math.random()-0.5)*6;
+    }
+
+    update(){
+
+        this.x += this.dx;
+        this.y += this.dy;
+
+        if(this.y<0 || this.y>canvas.height)
+            this.dy *= -1;
+    }
+
+    draw(ctx){
+
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,this.radius,0,Math.PI*2);
+        ctx.fillStyle="white";
+        ctx.fill();
+
+    }
+
+}
