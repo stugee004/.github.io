35b639938f7258c979fb4ab42a25e59e766e002f
stugee004 <stugee004@gmail.com>
2026-07-14 11:46:46 -0700

    Update stars.js

--- stars.js
+++ stars.js
@@ -18,10 +18,39 @@ class Starfield {
                 speed: Math.random() * 0.01 + 0.003,
 
                 direction: Math.random() > 0.5 ? 1 : -1
-
             });
 
         }
+
+
+        this.constellation = null;
+        this.constellationTimer = 0;
+
+
+        // 5% chance when game starts
+        if(Math.random() < 0.05){
+            this.spawnConstellation();
+        }
+
+    }
+
+
+    spawnConstellation(){
+
+        // Big Dipper shape
+        this.constellation = [
+
+            {x:700,y:120},
+            {x:760,y:100},
+            {x:820,y:120},
+            {x:870,y:170},
+            {x:920,y:210},
+            {x:860,y:250},
+            {x:800,y:240}
+
+        ];
+
+        this.constellationTimer = 900;
 
     }
 
@@ -50,14 +79,36 @@ class Starfield {
 
         });
 
+
+        if(this.constellation){
+
+            this.constellationTimer--;
+
+            if(this.constellationTimer <= 0){
+
+                this.constellation = null;
+
+            }
+
+        }
+
+
+        // Small chance for a new constellation
+        if(!this.constellation && Math.random() < 0.0005){
+
+            this.spawnConstellation();
+
+        }
+
     }
 
 
     draw(ctx){
 
-        ctx.fillStyle = "white";
+        ctx.fillStyle="white";
 
-        this.stars.forEach(star => {
+
+        this.stars.forEach(star=>{
 
             ctx.globalAlpha = star.brightness;
 
@@ -68,14 +119,66 @@ class Starfield {
                 star.y,
                 star.size,
                 0,
-                Math.PI * 2
+                Math.PI*2
             );
 
             ctx.fill();
 
         });
 
-        ctx.globalAlpha = 1;
+
+        // Draw constellation
+        if(this.constellation){
+
+            ctx.globalAlpha = 0.5;
+
+            ctx.strokeStyle="white";
+            ctx.lineWidth=1;
+
+
+            ctx.beginPath();
+
+            this.constellation.forEach((star,index)=>{
+
+                if(index===0){
+
+                    ctx.moveTo(star.x,star.y);
+
+                } else {
+
+                    ctx.lineTo(star.x,star.y);
+
+                }
+
+            });
+
+
+            ctx.stroke();
+
+
+            // constellation stars
+            ctx.globalAlpha=1;
+
+            this.constellation.forEach(star=>{
+
+                ctx.beginPath();
+
+                ctx.arc(
+                    star.x,
+                    star.y,
+                    4,
+                    0,
+                    Math.PI*2
+                );
+
+                ctx.fill();
+
+            });
+
+        }
+
+
+        ctx.globalAlpha=1;
 
     }
 
