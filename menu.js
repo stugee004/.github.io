ea550d7a776cef4c026fd5c9ea40d0450ec1be91
stugee004 <stugee004@gmail.com>
2026-07-14 16:07:47 -0700

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,306 @@
+class MenuManager {
+
+
+    constructor(){
+
+
+        this.screen = "menu";
+
+
+        this.createMenu();
+
+
+    }
+
+
+
+
+
+    createMenu(){
+
+
+        const menu =
+            document.createElement("div");
+
+
+        menu.id = "mainMenu";
+
+
+        menu.innerHTML = `
+
+            <h1>HYPER PONG</h1>
+
+            <div id="cenes">
+                0
+            </div>
+
+
+            <button id="playButton">
+                PLAY
+            </button>
+
+
+            <button id="difficultyButton">
+                Difficulty: Medium
+            </button>
+
+
+            <button id="shopButton">
+                Shop
+            </button>
+
+
+            <button id="abilitiesButton">
+                Abilities
+            </button>
+
+        `;
+
+
+
+        document.body.appendChild(menu);
+
+
+
+        this.menu =
+            menu;
+
+
+
+        this.connectButtons();
+
+
+    }
+
+
+
+
+
+
+    connectButtons(){
+
+
+
+        document
+        .getElementById(
+            "playButton"
+        )
+        .onclick = ()=>{
+
+
+            this.startGame();
+
+
+        };
+
+
+
+
+
+        document
+        .getElementById(
+            "difficultyButton"
+        )
+        .onclick = ()=>{
+
+
+            this.changeDifficulty();
+
+
+        };
+
+
+
+
+
+        document
+        .getElementById(
+            "shopButton"
+        )
+        .onclick = ()=>{
+
+
+            this.openShop();
+
+
+        };
+
+
+
+
+
+        document
+        .getElementById(
+            "abilitiesButton"
+        )
+        .onclick = ()=>{
+
+
+            this.openAbilities();
+
+
+        };
+
+
+    }
+
+
+
+
+
+
+
+    startGame(){
+
+
+        this.screen =
+            "game";
+
+
+        this.menu.style.display =
+            "none";
+
+
+
+        if(typeof resetGame === "function"){
+
+            resetGame();
+
+        }
+
+
+        gameStarted = true;
+
+
+    }
+
+
+
+
+
+
+
+    changeDifficulty(){
+
+
+        let current =
+            save.get(
+                "settings.difficulty"
+            );
+
+
+
+        let next;
+
+
+
+        if(current==="easy"){
+
+            next="medium";
+
+        }
+
+        else if(current==="medium"){
+
+            next="hard";
+
+        }
+
+        else{
+
+            next="easy";
+
+        }
+
+
+
+
+        save.set(
+            "settings.difficulty",
+            next
+        );
+
+
+
+        document
+        .getElementById(
+            "difficultyButton"
+        )
+        .innerText =
+            "Difficulty: " +
+            next;
+
+
+    }
+
+
+
+
+
+
+
+    openShop(){
+
+
+        console.log(
+            "Shop opened"
+        );
+
+
+        // shop UI will connect here
+
+
+    }
+
+
+
+
+
+
+
+    openAbilities(){
+
+
+        console.log(
+            "Abilities opened"
+        );
+
+
+    }
+
+
+
+
+
+    show(){
+
+
+        this.menu.style.display =
+            "block";
+
+
+    }
+
+
+
+
+
+    hide(){
+
+
+        this.menu.style.display =
+            "none";
+
+
+    }
+
+
+
+}
+
+
+
+
+
+const menu =
+    new MenuManager();
