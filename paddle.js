57cc9edfbd88b8a4c7bd25235e9b830b9f0c3d1e
stugee004 <stugee004@gmail.com>
2026-07-14 13:19:50 -0700

    Update paddle.js

--- paddle.js
+++ paddle.js
@@ -31,45 +31,83 @@ class Paddle {
     }
 
 
-draw(ctx){
+class Paddle {
 
-    // Glow
-    ctx.shadowBlur = 20;
-    ctx.shadowColor = this.color;
+    constructor(x, y, color = "cyan") {
 
+        this.x = x;
+        this.y = y;
 
-    // Gradient paddle
-    let gradient = ctx.createLinearGradient(
-        this.x,
-        this.y,
-        this.x + this.width,
-        this.y
-    );
+        this.width = 25;
+        this.height = 120;
 
+        this.speed = 7;
+        this.velocity = 0;
 
-    gradient.addColorStop(0, this.color);
-    gradient.addColorStop(0.5, "white");
-    gradient.addColorStop(1, this.color);
+        this.color = color;
+
+    }
 
 
-    ctx.fillStyle = gradient;
+    update() {
+
+        this.y += this.velocity;
 
 
-    // Rounded paddle
-    ctx.beginPath();
-
-    ctx.roundRect(
-        this.x,
-        this.y,
-        this.width,
-        this.height,
-        15
-    );
-
-    ctx.fill();
+        // Keep paddle on screen
+        if(this.y < 0) {
+            this.y = 0;
+        }
 
 
-    // Reset glow
-    ctx.shadowBlur = 0;
+        if(this.y + this.height > canvas.height) {
+            this.y = canvas.height - this.height;
+        }
+
+    }
+
+
+    draw(ctx) {
+
+        // Glow
+        ctx.shadowBlur = 20;
+        ctx.shadowColor = this.color;
+
+
+        // Gradient
+        let gradient = ctx.createLinearGradient(
+            this.x,
+            this.y,
+            this.x + this.width,
+            this.y
+        );
+
+
+        gradient.addColorStop(0, this.color);
+        gradient.addColorStop(0.5, "white");
+        gradient.addColorStop(1, this.color);
+
+
+        ctx.fillStyle = gradient;
+
+
+        // Rounded paddle
+        ctx.beginPath();
+
+        ctx.roundRect(
+            this.x,
+            this.y,
+            this.width,
+            this.height,
+            12
+        );
+
+        ctx.fill();
+
+
+        // Turn glow off
+        ctx.shadowBlur = 0;
+
+    }
 
 }
