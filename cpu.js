9bb884354b93570e9024f73343e786434d7a4a99
stugee004 <stugee004@gmail.com>
2026-07-14 11:05:18 -0700

    Update cpu.js

--- cpu.js
+++ cpu.js
@@ -2,44 +2,50 @@ class CPU extends Paddle {
 
     constructor(x, y) {
         super(x, y);
-
         this.speed = 7;
     }
 
-    update(ball){
+    update(ball) {
 
-        // If the ball is moving away, return to center
-        if(ball.dx < 0){
+        // Return to center if the ball is moving away
+        if (ball.dx < 0) {
 
             const target = canvas.height / 2;
 
-            if(this.y + this.height/2 < target)
+            if (this.y + this.height / 2 < target) {
                 this.y += this.speed;
-            else
+            } else {
                 this.y -= this.speed;
+            }
 
-            return;
+        } else {
+
+            // Predict where the ball will arrive
+            let predictedY = ball.y +
+                (this.x - ball.x) * (ball.dy / ball.dx);
+
+            // Add some inaccuracy
+            predictedY += (Math.random() - 0.5) * 50;
+
+            if (this.y + this.height / 2 < predictedY) {
+                this.y += this.speed;
+            }
+
+            if (this.y + this.height / 2 > predictedY) {
+                this.y -= this.speed;
+            }
+
         }
 
-        // Predict where the ball will cross the CPU's x-position
-        let predictedY = ball.y +
-            (this.x - ball.x) * (ball.dy / ball.dx);
-
-        // Add a small random error so it isn't perfect
-        predictedY += (Math.random() - 0.5) * 50;
-
-        if(this.y + this.height/2 < predictedY)
-            this.y += this.speed;
-
-        if(this.y + this.height/2 > predictedY)
-            this.y -= this.speed;
-
         // Keep paddle on screen
-        if(this.y < 0)
+        if (this.y < 0) {
             this.y = 0;
+        }
 
-        if(this.y + this.height > canvas.height)
+        if (this.y + this.height > canvas.height) {
             this.y = canvas.height - this.height;
+        }
+
     }
-}
+
 }
