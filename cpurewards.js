648b10447d461b7fdcbd1fc4eddc5cb44c1bd9e8
stugee004 <stugee004@gmail.com>
2026-07-14 10:22:51 -0700

    Create cpurewards.js

---
+++ cpurewards.js
@@ -0,0 +1,28 @@
+const difficulties = {
+
+    easy:{
+        speed:4,
+        reward:50
+    },
+
+    medium:{
+        speed:6,
+        reward:120
+    },
+
+    hard:{
+        speed:8,
+        reward:250
+    },
+
+    expert:{
+        speed:10,
+        reward:500
+    },
+
+    impossible:{
+        speed:14,
+        reward:1000
+    }
+
+};
