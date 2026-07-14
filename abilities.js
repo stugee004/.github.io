c56678971a34cff47182ab35147c20529c7769b0
stugee004 <stugee004@gmail.com>
2026-07-14 10:23:27 -0700

    Create abilities.js

---
+++ abilities.js
@@ -0,0 +1,43 @@
+const shop = [
+
+{
+    id:"dash",
+
+    name:"Dash",
+
+    price:300
+},
+
+{
+    id:"freeze",
+
+    name:"Freeze Ball",
+
+    price:900
+},
+
+{
+    id:"curve",
+
+    name:"Curve Shot",
+
+    price:1800
+},
+
+{
+    id:"shield",
+
+    name:"Goal Shield",
+
+    price:2500
+},
+
+{
+    id:"teleport",
+
+    name:"Teleport",
+
+    price:4000
+}
+
+];
