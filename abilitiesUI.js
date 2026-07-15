2cde8de3183f8455ed5801b85cc08c434f57c251
stugee004 <stugee004@gmail.com>
2026-07-14 18:37:01 -0700

    Create abilitiesUI.js

---
+++ abilitiesUI.js
@@ -0,0 +1,340 @@
+class AbilitiesUI {
+
+
+    constructor(){
+
+
+        this.visible = false;
+
+
+        this.create();
+
+
+    }
+
+
+
+
+
+
+
+    create(){
+
+
+
+        const abilities =
+            document.createElement("div");
+
+
+
+        abilities.id = "abilitiesUI";
+
+
+        abilities.style.display =
+            "none";
+
+
+
+
+        abilities.innerHTML = `
+
+
+            <h2>ABILITIES</h2>
+
+
+
+            <div id="abilitiesBalance">
+                Cenes: 0
+            </div>
+
+
+
+            <div id="abilityItems"></div>
+
+
+
+            <button id="closeAbilities">
+                Back
+            </button>
+
+
+
+        `;
+
+
+
+        document.body.appendChild(
+            abilities
+        );
+
+
+
+        this.abilities =
+            abilities;
+
+
+
+
+
+        document
+        .getElementById(
+            "closeAbilities"
+        )
+        .onclick = ()=>{
+
+
+            this.close();
+
+
+        };
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+    open(){
+
+
+
+        this.visible = true;
+
+
+
+        this.abilities.style.display =
+            "block";
+
+
+
+        if(menu){
+
+
+            menu.hide();
+
+
+        }
+
+
+
+        this.refresh();
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+    close(){
+
+
+
+        this.visible = false;
+
+
+
+        this.abilities.style.display =
+            "none";
+
+
+
+        if(menu){
+
+
+            menu.show();
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+    refresh(){
+
+
+
+        let balance = 0;
+
+
+
+        if(typeof economy !== "undefined"){
+
+
+            balance =
+                economy.cenes;
+
+
+        }
+
+
+
+
+
+        document
+        .getElementById(
+            "abilitiesBalance"
+        )
+        .innerText =
+
+            "Cenes: "
+            +
+            balance;
+
+
+
+
+
+
+
+        const container =
+            document.getElementById(
+                "abilityItems"
+            );
+
+
+
+        container.innerHTML = "";
+
+
+
+
+
+
+
+        if(typeof abilities === "undefined"){
+
+
+            return;
+
+
+        }
+
+
+
+
+
+
+
+        Object.keys(
+            abilities.abilities
+        )
+        .forEach(key=>{
+
+
+
+            const ability =
+                abilities.abilities[key];
+
+
+
+
+
+            const button =
+                document.createElement(
+                    "button"
+                );
+
+
+
+
+
+
+            button.innerText =
+
+                ability.name
+                +
+                " - "
+                +
+                ability.cost
+                +
+                " Cenes";
+
+
+
+
+
+
+
+
+            if(ability.unlocked){
+
+
+
+                button.innerText +=
+                    " ✓ Unlocked";
+
+
+
+                button.disabled =
+                    true;
+
+
+
+            }
+
+
+
+
+
+
+
+            button.onclick = ()=>{
+
+
+
+                abilities.unlock(key);
+
+
+
+                this.refresh();
+
+
+
+            };
+
+
+
+            container.appendChild(
+                button
+            );
+
+
+
+        });
+
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+const abilitiesUI =
+    new AbilitiesUI();
