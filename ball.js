80c05226650ce5b1c944c1d055d58970918fe55e
stugee004 <stugee004@gmail.com>
2026-07-14 17:40:32 -0700

    Update ball.js

--- ball.js
+++ ball.js
@@ -1,10 +1,13 @@
 class Ball {
 
+
     constructor(){
+
 
         this.radius = 10;
 
         this.speed = 7;
+
 
         this.x = canvas.width / 2;
         this.y = canvas.height / 2;
@@ -20,130 +23,41 @@ class Ball {
         this.trailTimer = 0;
 
 
+        this.hitCooldown = 0;
+
+
         this.reset();
 
+
     }
+
+
+
 
 
 
     reset(){
 
+
         this.x = canvas.width / 2;
+
         this.y = canvas.height / 2;
+
 
 
         this.speed = 7;
 
 
-        // Random starting direction
 
         const direction =
             Math.random() > 0.5 ? 1 : -1;
 
 
-        const angle =
-            (Math.random() - 0.5) * Math.PI / 3;
-
-
-        this.dx =
-            Math.cos(angle) *
-            this.speed *
-            direction;
-
-
-        this.dy =
-            Math.sin(angle) *
-            this.speed;
-
-    }
-
-
-
-    update(){
-
-
-        this.x += this.dx;
-
-        this.y += this.dy;
-
-
-
-        // Top and bottom bounce
-
-        if(this.y - this.radius <= 0){
-
-            this.y = this.radius;
-
-            this.dy *= -1;
-
-
-            this.wallSound();
-
-        }
-
-
-
-        if(this.y + this.radius >= canvas.height){
-
-            this.y =
-                canvas.height - this.radius;
-
-
-            this.dy *= -1;
-
-
-            this.wallSound();
-
-        }
-
-
-
-        // Trail particles
-
-        this.trailTimer++;
-
-
-        if(
-            this.trailTimer % 2 === 0 &&
-            typeof particles !== "undefined"
-        ){
-
-            particles.ballTrail(this);
-
-        }
-
-
-    }
-
-
-
-    bounce(paddle){
-
-
-        // Distance from paddle center
-
-        const hitPosition =
-            (this.y -
-            (paddle.y + paddle.height/2))
-            /
-            (paddle.height/2);
-
-
-
-        // Limit extreme angles
 
         const angle =
-            hitPosition *
+            (Math.random() - 0.5)
+            *
             Math.PI / 3;
-
-
-
-        const direction =
-            this.dx > 0 ? -1 : 1;
-
-
-
-        this.speed += 0.25;
 
 
 
@@ -163,27 +77,226 @@ class Ball {
 
 
 
-        // Effect
+        this.hitCooldown = 0;
+
+
+    }
+
+
+
+
+
+
+
+
+    update(){
+
+
+
+        this.x += this.dx;
+
+        this.y += this.dy;
+
+
+
+        if(this.hitCooldown > 0){
+
+            this.hitCooldown--;
+
+        }
+
+
+
+
+
+
+        // Top wall
+
+
+        if(
+            this.y - this.radius <= 0
+        ){
+
+
+            this.y = this.radius;
+
+
+            this.dy *= -1;
+
+
+            this.playWallSound();
+
+
+        }
+
+
+
+
+
+
+        // Bottom wall
+
+
+        if(
+            this.y + this.radius >= canvas.height
+        ){
+
+
+            this.y =
+                canvas.height - this.radius;
+
+
+
+            this.dy *= -1;
+
+
+            this.playWallSound();
+
+
+        }
+
+
+
+
+
+
+
+        this.trailTimer++;
+
+
+
+
+        if(
+            this.trailTimer % 2 === 0 &&
+            typeof particles !== "undefined"
+        ){
+
+
+            particles.ballTrail(this);
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+    bounce(paddle){
+
+
+
+        if(this.hitCooldown > 0){
+
+            return;
+
+        }
+
+
+
+        const hitPosition =
+            (
+                this.y -
+                (
+                    paddle.y +
+                    paddle.height / 2
+                )
+            )
+            /
+            (
+                paddle.height / 2
+            );
+
+
+
+
+
+        const angle =
+            hitPosition *
+            Math.PI / 3;
+
+
+
+
+
+        const direction =
+            this.dx > 0 ? -1 : 1;
+
+
+
+
+
+        this.speed += 0.25;
+
+
+
+
+
+        this.dx =
+            Math.cos(angle)
+            *
+            this.speed
+            *
+            direction;
+
+
+
+        this.dy =
+            Math.sin(angle)
+            *
+            this.speed;
+
+
+
+
+
+
+
+        this.hitCooldown = 8;
+
+
+
+
 
         if(typeof particles !== "undefined"){
 
             particles.paddleHit(
+
                 this.x,
+
                 this.y,
+
                 paddle.color
+
             );
 
         }
 
 
 
-        if(typeof playHit === "function"){
+
+
+
+        if(typeof sounds !== "undefined"){
 
             sounds.hit();
 
         }
 
+
+
     }
+
+
+
+
+
 
 
 
@@ -191,36 +304,62 @@ class Ball {
     checkPaddleCollision(paddle){
 
 
+
         if(
+
 
             this.x + this.radius > paddle.x &&
 
-            this.x - this.radius < paddle.x + paddle.width &&
 
-            this.y + this.radius > paddle.y &&
+            this.x - this.radius <
+            paddle.x + paddle.width &&
 
-            this.y - this.radius < paddle.y + paddle.height
+
+            this.y + this.radius >
+            paddle.y &&
+
+
+            this.y - this.radius <
+            paddle.y + paddle.height
+
+
 
         ){
 
+
             this.bounce(paddle);
 
+
         }
+
+
 
     }
 
 
 
 
-    wallSound(){
 
-        if(typeof playWall === "function"){
+
+
+
+
+    playWallSound(){
+
+
+
+        if(typeof sounds !== "undefined"){
 
             sounds.wall();
 
         }
 
+
     }
+
+
+
+
 
 
 
@@ -229,7 +368,9 @@ class Ball {
     draw(ctx){
 
 
+
         ctx.save();
+
 
 
         ctx.shadowBlur = 25;
@@ -237,56 +378,79 @@ class Ball {
         ctx.shadowColor = "cyan";
 
 
+
         ctx.fillStyle = this.color;
 
 
 
+
         ctx.beginPath();
+
 
 
         ctx.arc(
 
             this.x,
+
             this.y,
+
             this.radius,
+
             0,
-            Math.PI*2
+
+            Math.PI * 2
 
         );
+
 
 
         ctx.fill();
 
 
 
-        // Bright center
+
+
+
 
         ctx.shadowBlur = 0;
 
-        ctx.fillStyle="white";
+
+
+        ctx.fillStyle = "white";
+
 
 
         ctx.beginPath();
 
 
+
         ctx.arc(
 
             this.x,
+
             this.y,
-            this.radius/2,
+
+            this.radius / 2,
+
             0,
-            Math.PI*2
+
+            Math.PI * 2
 
         );
 
 
+
         ctx.fill();
+
 
 
 
         ctx.restore();
 
 
+
     }
+
+
 
 }
